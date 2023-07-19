cffd4e0a675cf45eac12cc4513326ed6323b82de
mysCod3r <m.yasinsenocak@gmail.com>
2023-07-20 01:42:26 +0300

    db context

---
+++ Data/CelebiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CelebiWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CelebiWebApi.Data
+{
+    public class CelebiDbContext : DbContext
+    {
+        public CelebiDbContext(DbContextOptions<CelebiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Uye> Uye { get; set; }
+        public DbSet<Uye> Brans { get; set; }
+        public DbSet<Uye> AltBrans { get; set; }
+        public DbSet<Uye> Personel { get; set; }
+        public DbSet<Uye> UyePaket { get; set; }
+    }
+}
