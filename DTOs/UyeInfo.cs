f68f4bda0605db42ac10f7ec86c1fcdd92103464
mysCod3r <m.yasinsenocak@gmail.com>
2023-07-20 01:43:33 +0300

    DTOs

---
+++ DTOs/UyeInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace CelebiWebApi.DTOs
+{
+    public class UyeInfo
+    {
+        public string? TC { get; set; }
+        public string? AdSoyad { get; set; }
+        public string? AnneAdi { get; set; }
+        public string? BabaAdi { get; set; }
+        public string? AnneTelefon { get; set; }
+        public string? BabaTelefon { get; set; }
+        public string? FotoUrl { get; set; }
+        public string? Brans { get; set; }
+        public string? AltBrans { get; set; }
+        public string? Kusak { get; set; }
+        public string? Antrenor { get; set; }
+        public DateTime? KayitTarihi { get; set; }
+        public string? Durum { get; set; }
+        public string? LisansNo { get; set; }
+        public int? LisansYili { get; set; }
+        public int? AidatTutari { get; set; }
+    }
+}
